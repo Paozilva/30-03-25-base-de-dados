54928b6737d0c10934ad445a9680c5a940486a47
Paozilva <asesoriadetareas315@gmail.com>
2025-04-01 13:54:44 -0300

    Add files via upload

---
+++ create-databased.sql
@@ -0,0 +1,4 @@
+create database `silva`;
+# a linha acima serve para criar um banco de dados com o nome `silva`
+# ouso da crease se torna necessário para incicar uma `string`do sistema
+# `strings` so sitema: `database`, `table` e `campo`
---
+++ insert.sql
@@ -0,0 +1,7 @@
+insert into `silva`.`tbl_login`
+(`nome`, `login`, `senha`)
+values ('teste', 'teste@teste','mudar123');
+# o uso da crase serve para indicar uma `strimg` de sistema
+# (`database`, `table`, `field`ou `campo`)
+# o uso de aspas simples serve para indicar uma `string`de valor 
+# (`na string de valor pode existir espaços)
